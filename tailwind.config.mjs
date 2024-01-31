285029dea69eb7790aec54bc540ea00eaeca8f9b
dimaspng <d.sagachko@gmail.com>
2024-01-31 18:56:19 +0200

    add initial mobile-responsive version of the first screen

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+				backgroundImage: {
+						'image-desktop': "url('src/assets/images/mainPage/mainBackground.avif')",
+						'image-mobile': "url('src/assets/images/mainPage/mainMin.avif')"
+				},
+				fontFamily: {
+						sans: ["GT Eesti Pro Display", ...defaultTheme.fontFamily.sans],
+						'pt-sans': ["PT Sans", ...defaultTheme.fontFamily.sans],
+				},
+		},
 	},
 	plugins: [],
 	darkMode: 'class'

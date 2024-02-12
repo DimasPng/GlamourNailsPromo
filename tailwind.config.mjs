b6c4bafb7d6ebe74d8d4339d59237b35bc86c8f3
NataliiaDMBRVSK <natadmbrvsk@gmail.com>
2024-02-12 19:07:36 +0200

    Edited form accoding to design

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -6,7 +6,9 @@ export default {
 		extend: {
 				backgroundImage: {
 						'image-desktop': "url('src/assets/images/mainPage/mainBackground.avif')",
-						'image-mobile': "url('src/assets/images/mainPage/mainMin.avif')"
+						'image-mobile': "url('src/assets/images/mainPage/mainMin.avif')",
+						'image-desktop-question': "url('src/assets/images/mainPage/background-for-question-section.avif')",
+						'image-mobile-question': "url('src/assets/images/mainPage/background-for-question-section-mobile.avif')"
 				},
 				fontFamily: {
 						sans: ["GT Eesti Pro Display", ...defaultTheme.fontFamily.sans],
